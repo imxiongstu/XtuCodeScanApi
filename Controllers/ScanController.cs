eb41a6de750065412f58490e3352a306215630d7
小泽又沐风Σ( ﾟωﾟ <oncwnuKQKOPq4vP2ptHVqKvGnzJw@git.weixin.qq.com>
2021-01-06 14:26:14 +0800

    所有api的文件

---
+++ Controllers/ScanController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Threading.Tasks;
+using ZXing;
+using System.IO;
+using XtuScan;
+using Microsoft.AspNetCore.Hosting;
+using ZXing.Common;
+
+namespace QrCodeScanAPI.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    [EnableCors("DIYCors")]
+    public class ScanController : ControllerBase
+    {
+
+        [HttpPost]
+        public string AnalysisQrCode()
+        {
+            if (HttpContext.Request.Form.Files.Count <= 0) return "没有选择文件";
+
+            var imgFile = HttpContext.Request.Form.Files[0];
+
+            try
+            {
+                Image imagePic = Image.FromStream(imgFile.OpenReadStream());
+                return QrCodeUtils.AnalysisQrCode(imagePic);
+            }
+            catch (Exception)
+            {
+                return "识别失败，请重新调整";
+            }
+        }
+    }
+}
+
---
+++ Startup.cs
@@ -0,0 +1,56 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QrCodeScanAPI
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddCors();
+            //允许所有源跨域
+            services.AddCors(options =>
+                options.AddPolicy("DIYCors",
+                p =>
+                p.AllowAnyOrigin().AllowAnyHeader()
+                )
+            );
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+            app.UseCors();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
